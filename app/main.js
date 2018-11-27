bebdcbeabb2c1abc0775e65b9fce6993c11e700f
rodrigo debian <correo.rodrigo@gmail.com>
2018-11-27 14:57:47 -0600

    black background issue

--- app/main.js
+++ app/main.js
@@ -1,7 +1,12 @@
 const {app, BrowserWindow} = require ('electron');
 let mainWindow = null;
+if (process.platform === 'linux') {
+  app.disableHardwareAcceleration();
+}
 
 app.on('ready', () => {
-  console.log ('Hello from Electron');
   mainWindow = new BrowserWindow();
+  //mainWindow.webContents.loadFile('file://' + __dirname + '/index.html');
+  //mainWindow.webContents.loadFile('index.html');
+  mainWindow.webContents.loadFile('./index.html');
 })
