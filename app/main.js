bea858085162e1f3d8471da4e5dc824f327b52e6
rodrigo debian <correo.rodrigo@gmail.com>
2018-11-27 15:07:49 -0600

    fixes

--- app/main.js
+++ app/main.js
@@ -1,12 +1,8 @@
 const {app, BrowserWindow} = require ('electron');
 let mainWindow = null;
-if (process.platform === 'linux') {
-  app.disableHardwareAcceleration();
-}
+
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow();
-  //mainWindow.webContents.loadFile('file://' + __dirname + '/index.html');
-  //mainWindow.webContents.loadFile('index.html');
-  mainWindow.webContents.loadFile('./index.html');
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
