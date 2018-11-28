8d06900c30fba9fbcc659930be971012889ae89c
rodrigo debian <correo.rodrigo@gmail.com>
2018-11-28 07:31:49 -0600

    styles

--- app/main.js
+++ app/main.js
@@ -3,6 +3,14 @@ let mainWindow = null;
 
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow = new BrowserWindow({ show: false });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 })
