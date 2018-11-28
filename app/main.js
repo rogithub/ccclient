ddc7bb3baa82f4f62a8085a7de9904fabf694549
rodrigo debian <correo.rodrigo@gmail.com>
2018-11-28 14:13:02 -0600

    adding one menu

--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
-const {app, BrowserWindow, ipcMain } = require ('electron');
+const {app, BrowserWindow, ipcMain, Menu } = require ('electron');
 let mainWindow = null;
+
+const menuTemplate = [{
+  label: 'Archivo'
+}];
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ show: false });
-
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
