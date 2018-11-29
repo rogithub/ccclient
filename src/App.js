081593b61a2fb05d5111c7701c0138df2019b7a4
rodrigo debian <correo.rodrigo@gmail.com>
2018-11-29 16:26:24 -0600

    removing warnings

--- src/App.js
+++ src/App.js
@@ -7,37 +7,37 @@ class LeftMenu extends Component {
         <div className="sidebar-sticky">
           <ul className="nav flex-column">
             <li className="nav-item">
-              <a className="nav-link active" href="#">
+              <a className="nav-link active" href="/LibroDiario">
                 <i className="fas fa-pencil-alt"></i>
                 &nbsp;Libro Diario <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/Presupuestos">
                 <i className="far fa-file-alt"></i>
                 &nbsp;Presupuestos
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/Productos">
                 <i className="fas fa-shopping-cart"></i>
                 &nbsp;Productos
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/Clientes">
                 <i className="fas fa-users"></i>
                 &nbsp;Clientes
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/Proveedores">
                 <i className="fas fa-dolly"></i>
                 &nbsp;Proveedores
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/Inventario">
                 <i className="fas fa-clipboard-list"></i>
                 &nbsp;Inventario
               </a>
