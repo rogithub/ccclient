08c87435bd81259445ef15e7c48c8201673f8b47
rodrigo debian <correo.rodrigo@gmail.com>
2018-12-10 14:42:51 -0600

    styling

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 
 export default props =>
   <div>
     <Divider component="div" />
-    <Typography align="center" variant="subheading">
-      Confecciones Colombia
-    </Typography>
   </div>
