bdd47214742a441e4cf0ef95f2043f0f59f2555c
rodrigo debian <correo.rodrigo@gmail.com>
2019-01-31 19:07:33 -0600

    this needs more testing

--- src/apis/cc.js
+++ src/apis/cc.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  //baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: "http://roazure.southcentralus.cloudapp.azure.com:8000",
   transformRequest: [(data) => JSON.stringify(data || undefined)],
   headers: {
     'Accept': 'application/json',
