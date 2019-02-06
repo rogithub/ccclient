82465734685d4e4e05a315c5d5ea33a044645866
rodrigo debian <correo.rodrigo@gmail.com>
2019-02-06 17:20:46 -0600

    remove config

--- src/apis/cc.js
+++ src/apis/cc.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  //baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
-  //baseURL: "http://roazure.southcentralus.cloudapp.azure.com:8000",
   baseURL: "http://localhost:8000",
   transformRequest: [(data) => JSON.stringify(data || undefined)],
   headers: {
