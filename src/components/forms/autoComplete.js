e6773f163f1fe11291dddeac63f2877f29dedd45
rodrigo debian <correo.rodrigo@gmail.com>
2019-02-11 15:23:43 -0600

    extracting out methods

--- src/components/forms/autoComplete.js
+++ src/components/forms/autoComplete.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import deburr from 'lodash/deburr';
 import Autosuggest from 'react-autosuggest';
 import match from 'autosuggest-highlight/match';
 import parse from 'autosuggest-highlight/parse';
@@ -9,43 +8,6 @@ import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import Popper from '@material-ui/core/Popper';
 import { withStyles } from '@material-ui/core/styles';
-
-const suggestions = [
-  { label: 'Afghanistan' },
-  { label: 'Aland Islands' },
-  { label: 'Albania' },
-  { label: 'Algeria' },
-  { label: 'American Samoa' },
-  { label: 'Andorra' },
-  { label: 'Angola' },
-  { label: 'Anguilla' },
-  { label: 'Antarctica' },
-  { label: 'Antigua and Barbuda' },
-  { label: 'Argentina' },
-  { label: 'Armenia' },
-  { label: 'Aruba' },
-  { label: 'Australia' },
-  { label: 'Austria' },
-  { label: 'Azerbaijan' },
-  { label: 'Bahamas' },
-  { label: 'Bahrain' },
-  { label: 'Bangladesh' },
-  { label: 'Barbados' },
-  { label: 'Belarus' },
-  { label: 'Belgium' },
-  { label: 'Belize' },
-  { label: 'Benin' },
-  { label: 'Bermuda' },
-  { label: 'Bhutan' },
-  { label: 'Bolivia, Plurinational State of' },
-  { label: 'Bonaire, Sint Eustatius and Saba' },
-  { label: 'Bosnia and Herzegovina' },
-  { label: 'Botswana' },
-  { label: 'Bouvet Island' },
-  { label: 'Brazil' },
-  { label: 'British Indian Ocean Territory' },
-  { label: 'Brunei Darussalam' },
-];
 
 function renderInputComponent(inputProps) {
   const { classes, inputRef = () => {}, ref, ...other } = inputProps;
@@ -68,8 +30,8 @@ function renderInputComponent(inputProps) {
 }
 
 function renderSuggestion(suggestion, { query, isHighlighted }) {
-  const matches = match(suggestion.label, query);
-  const parts = parse(suggestion.label, matches);
+  const matches = match(suggestion.empresa, query);
+  const parts = parse(suggestion.empresa, matches);
 
   return (
     <MenuItem selected={isHighlighted} component="div">
@@ -88,29 +50,6 @@ function renderSuggestion(suggestion, { query, isHighlighted }) {
       </div>
     </MenuItem>
   );
-}
-
-function getSuggestions(value) {
-  const inputValue = deburr(value.trim()).toLowerCase();
-  const inputLength = inputValue.length;
-  let count = 0;
-
-  return inputLength === 0
-    ? []
-    : suggestions.filter(suggestion => {
-        const keep =
-          count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-        if (keep) {
-          count += 1;
-        }
-
-        return keep;
-      });
-}
-
-function getSuggestionValue(suggestion) {
-  return suggestion.label;
 }
 
 const styles = theme => ({
@@ -149,6 +88,7 @@ class AutoComplete extends React.Component {
   };
 
   handleSuggestionsFetchRequested = ({ value }) => {
+    const getSuggestions = this.props.getSuggestions;
     this.setState({
       suggestions: getSuggestions(value),
     });
@@ -167,7 +107,7 @@ class AutoComplete extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, getSuggestionValue, label, placeholder } = this.props;
 
     const autosuggestProps = {
       renderInputComponent,
@@ -184,8 +124,8 @@ class AutoComplete extends React.Component {
           {...autosuggestProps}
           inputProps={{
             classes,
-            label: 'Label',
-            placeholder: 'With Popper',
+            label: label,
+            placeholder: placeholder,
             value: this.state.popper,
             onChange: this.handleChange('popper'),
             inputRef: node => {
